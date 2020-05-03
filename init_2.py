0d22123f9b453bd0da55b47f12fa21f4f78ee390
jjliu159 <55606804+jjliu159@users.noreply.github.com>
2020-05-03 13:31:28 -0400

    update
    removed some code

--- init_2.py
+++ init_2.py
@@ -113,33 +113,6 @@ def home():
     cursor.close()
     return render_template('home.html', username=name, photos = new_photo) #posts=data)
 
-'''
-@app.route('/select_blogger')
-def select_blogger():
-    #check that user is logged in
-    #username = session['username']
-    #should throw exception if username not found
-    
-    cursor = conn.cursor();
-    query = 'SELECT DISTINCT username FROM blog'
-    cursor.execute(query)
-    data = cursor.fetchall()
-    cursor.close()
-    return render_template('select_blogger.html', user_list=data)
-'''
-
-'''
-@app.route('/show_posts', methods=["GET", "POST"])
-def show_posts():
-    poster = request.args['poster']
-    cursor = conn.cursor();
-    query = 'SELECT ts, blog_post FROM blog WHERE username = %s ORDER BY ts DESC'
-    cursor.execute(query, poster)
-    data = cursor.fetchall()
-    cursor.close()
-    return render_template('show_posts.html', poster_name=poster, posts=data)
-'''
-
 @app.route('/logout')
 def logout():
     session.pop('username')
